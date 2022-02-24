7c342c0fde26a003123466e8e76d6769b507cb7f
mbaddar <mohamed.baddar@here.com>
2022-02-24 09:02:01 +0100

    graphs

--- test.py
+++ test.py
@@ -1,5 +1,21 @@
+import matplotlib.pyplot as plt
 import pandas as pd
+
 filename = "data/trackLog-2022-Feb-20_18-45-32.csv"
-df = pd.read_csv(filename,sep=',')
+df = pd.read_csv(filename, sep=',')
 print(df.columns)
 print(df.shape)
+
+cols = [' Device Time', 'Kilometers Per Litre(Instant)(kpl)', 'Engine RPM(rpm)']
+df[cols].to_csv('./debug.csv', sep=',', index=False)
+
+# outliers
+idx = df['Kilometers Per Litre(Instant)(kpl)'] >= 20
+df.loc[idx, 'Kilometers Per Litre(Instant)(kpl)'] = 20
+
+df['Kilometers Per Litre(Instant)(kpl)'].plot(label='kpl', color='red')
+(df['Engine RPM(rpm)'] / 1000).plot(label='rpm', color='blue')
+
+plt.legend()
+plt.xlabel('Time')
+plt.savefig('plot.png')
